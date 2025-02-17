8024a6a855be691239bf15ef0ca299e0218741bc
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2025-02-17 15:49:10 +0100

    Update functions_and_more.py

--- functions_and_more.py
+++ functions_and_more.py
@@ -1,4 +1,4 @@
-# FUNZIONI
+# Funzioni
 
 def fai_la_pasta():  # definizione
     print("metti l'acqua")
@@ -70,3 +70,62 @@ def fai_somma(num1, num2):
 
 x = fai_somma(2, 2)
 print(x)
+
+
+
+#######
+
+# Classi e oggetti
+# programmazione a oggetti
+
+# la classe è un'astrazione di una cosa reale (es. persone). le classi hanno delle proprietà (es. nome e cognome)
+
+class persona:      # è come se fosse uno stampino
+    nome = "Luca"
+    cognome = "Rossi"
+
+persona1 = persona()   # tutti gli oggetti (persona1, persona2...) derivano dalla stessa classe
+persona2 = persona()
+
+# un'istanza è l'oggetto specifico. istanza = oggetto che deriva da uno stampino
+
+print(persona1.nome)  # così com'è è uguale a persona2
+
+
+# Costruttore (è una funzione che viene chiamata automaticamente)
+
+class Persona:
+    def __init__(self, nome, cognome):   # self è il riferimento a se stesso (ci permette di capire che istanza è)
+        self.nome = nome
+        self.cognome = cognome
+
+Persona1 = Persona("Luca", "Rossi")
+Persona2 = Persona("Marco", "Verdi")
+
+
+print(Persona1.nome)
+print(Persona2.nome)
+
+
+
+
+class Persona:
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
+
+    def saluta(self):
+        print("ciao sono " + self.nome)
+
+Persona1 = Persona("Luca", "Rossi")
+Persona2 = Persona("Marco", "Verdi")
+
+Persona1.saluta()
+Persona2.saluta()
+
+
+# il parametro self aiuta a capire di che istanza particolare si sta parlando
+
+Persona2.nome = "Maria"
+Persona2.saluta()
+
